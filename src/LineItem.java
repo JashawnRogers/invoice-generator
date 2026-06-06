428e93368ba2ecaee1d4a869651b9997e6e92c68
JashawnRogers <rogersjashawn20@gmail.com>
2026-06-06 16:51:11 -0700

    Created line item object

---
+++ src/LineItem.java
@@ -0,0 +1,51 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class LineItem {
+    private final String description;
+    private final int quantity;
+    private final BigDecimal unitPrice;
+
+    public LineItem(String description, int quantity, BigDecimal unitPrice) {
+        this.description = validateAndNormalizeText(description);
+
+        if (quantity > 0) {
+            this.quantity = quantity;
+        } else {
+            throw new RuntimeException("Line item's quantity must be greater than 0.");
+        }
+
+        if (unitPrice.compareTo(BigDecimal.ZERO) > 0) {
+            this.unitPrice = unitPrice;
+        } else {
+            throw new RuntimeException("Line item unit price must be greater than $0.");
+        }
+    }
+
+    public LineItem createNew(String description, int quantity, BigDecimal unitPrice) {
+        return new LineItem(description, quantity, unitPrice);
+    }
+
+    public String validateAndNormalizeText(String text) {
+        if (text == null || text.isBlank()) {
+            throw new RuntimeException("A line item description is required.");
+        }
+        return text.trim();
+    }
+
+    public BigDecimal calculateTotal(int quantity, BigDecimal unitPrice) {
+        return unitPrice.multiply(BigDecimal.valueOf(quantity)).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+}
